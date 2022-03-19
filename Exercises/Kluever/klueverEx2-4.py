4c054b613e33dcb597d3af721c21dff80f4969e9
dezwemmer <56605591+dezwemmer@users.noreply.github.com>
2022-03-18 22:14:09 -0700

    added 2-4, needs edits on FPA quadrant

---
+++ Exercises/Kluever/klueverEx2-4.py
@@ -0,0 +1,36 @@
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# Orbital Mechanics & Dynamics Software Collection
+# Author:   Steven Anderson
+# Created:  Mar 2022
+# Brief:    Example 2.4
+#           Apollo CM tracked by Earth-based radar station
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+import math
+import constantsKluever as const
+
+# Givens:
+semiMajorAxis = 424587 #km
+ecc = 0.9849
+
+# (a) flight path angle and radial velocity @ TA 330
+trueAnomaly = math.radians(330) #radians
+flightPathAngle = math.degrees(math.atan( (ecc*math.sin(trueAnomaly))/(1 + ecc * math.cos(trueAnomaly)) ))
+param = semiMajorAxis*(1 - ecc**2)
+angMom = math.sqrt( const.mu*param )
+velRad = ( const.mu/angMom )*ecc*math.sin( trueAnomaly )
+print("~At FPA 330 degrees~")
+print('  Flight Path Angle: {:.1f} [degrees]'.format(flightPathAngle))
+print('  Radial Velocity: {:.3f} [km/s]'.format(velRad))
+
+# (b) calculate velocity and flight path angle at the entry interface (EI) or 122 km altitude
+totSpecEnergy = - const.mu / ( 2*semiMajorAxis)
+radEI = const.rE + 122
+velEI = math.sqrt( 2*( totSpecEnergy + ( const.mu/radEI ) ) )
+trueAnomalyEI = math.acos( (1/ecc)*( param/radEI - 1 ) )
+# fix this 
+print(math.degrees(trueAnomalyEI))
+flightPathAngleEI = math.degrees(math.atan( (ecc*math.sin(trueAnomalyEI))/(1 + ecc * math.cos(trueAnomalyEI)) ))
+print("~At Entry Interface (122km)~")
+print('  Velocity: {:.1f} [km/s]'.format(velEI))
+print('  Flight Path Angle: {:.3f} [degrees]'.format(flightPathAngleEI))
