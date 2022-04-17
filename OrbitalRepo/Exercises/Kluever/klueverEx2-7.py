49217182ea32b56db7f3b1853a5cb408778566e2
dezwemmer <56605591+dezwemmer@users.noreply.github.com>
2022-04-17 16:58:20 -0700

    finished 2.7

--- OrbitalRepo/Exercises/Kluever/klueverEx2-7.py
+++ OrbitalRepo/Exercises/Kluever/klueverEx2-7.py
@@ -9,16 +9,17 @@
 
 import math
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import constantsKluever as const
 
 #####
 # Givens:
 altPer = 500 #km 
 
-
 #####
 # Functions:
-def computeVel_TA(trueAnomalyDegrees, parameter):
+def computeRadVelfromTA(trueAnomalyDegrees, parameter):
     r = parameter / ( 1 + math.cos(math.radians(trueAnomalyDegrees)) )
     v = math.sqrt( 2*const.mu/r )
     return r,v 
@@ -28,14 +29,29 @@ def computeVel_TA(trueAnomalyDegrees, parameter):
 trueAnomalyList = [-60,0,90,150,179]
 radPer = const.rE + altPer
 param = 2*radPer
-trueAnomaly = -60
 
 outList = []
 for TA in trueAnomalyList:
-    rad,vel = computeVel_TA(TA,param)
+    rad,vel = computeRadVelfromTA(TA,param)
     print("At TA {:.1f}:".format(TA))
     print(" Rad: {:.3f} [km], Vel: {:.3f} [km/s]".format(rad,vel))
     outList.append([TA,rad,vel])
-# print vertically
+
+# print results vertically
 #print(np.vstack(outList))
 
+#####
+# (b) plot satellite velocity vs TA for -179<=TA<=179
+# create more data points
+TAList = []
+velList = []
+for i in range(-179,179,20):
+    TAList.append(i)
+    r,v = computeRadVelfromTA(i,param)
+    velList.append(v)
+
+fig,ax = plt.subplots()
+ax.plot(TAList,velList)
+ax.set_xlabel('True Anomaly [deg]')
+ax.set_ylabel('Velocity on a parabola [km/s]')
+plt.show()
